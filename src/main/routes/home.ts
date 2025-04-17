ab2f7b5d784cc84e275f6f710bfd0df625e2292c
nbesagni <36893225+nbesagni@users.noreply.github.com>
2025-04-18 00:39:18 +0100

    Connect task API and display task list

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,13 +1,32 @@
 import { Application } from 'express';
 import axios from 'axios';
+import { Task, TaskListData, TaskListItem } from '../types/task';
 
 export default function (app: Application): void {
   app.get('/', async (req, res) => {
     try {
-      // An example of connecting to the backend (a starting point)
-      const response = await axios.get('http://localhost:4000/get-example-case');
-      console.log(response.data);
-      res.render('home', { "example": response.data });
+      const response = await axios.get('http://localhost:4000/api/v1/tasks');
+      const tasks: Task[] = response.data;
+
+      const taskListItems: TaskListItem[] = tasks.map(task => ({
+        title: {
+          text: task.title
+        },
+        href: `#${task.id}`,
+        status: {
+          tag: {
+            text: task.status,
+            classes: task.status === 'Completed' ? 'govuk-tag--green' : 'govuk-tag--blue'
+          }
+        }
+      }));
+
+      const taskListData: TaskListData = {
+        idPrefix: "task-details",
+        items: taskListItems
+      };
+
+      res.render('home', { taskListData });
     } catch (error) {
       console.error('Error making request:', error);
       res.render('home', {});
