b7dca1ced961afa69789c7f00f72c948b2ba13bd
nbesagni <36893225+nbesagni@users.noreply.github.com>
2025-04-17 22:59:23 +0100

    Amended globl app error handling & glob function amended for Windows

--- src/main/app.ts
+++ src/main/app.ts
@@ -29,19 +29,31 @@ app.use((req, res, next) => {
   next();
 });
 
-glob
-  .sync(__dirname + '/routes/**/*.+(ts|js)')
-  .map(filename => require(filename))
-  .forEach(route => route.default(app));
+// Glob function amended to use path.posix.join for cross-platform compatibility
+const routes = glob.sync(path.posix.join(__dirname.replace(/\\/g, '/'), 'routes/**/*.+(ts|js)'));
+
+routes
+  .map(filename => {
+    const absolutePath = path.resolve(filename);
+    const route = require(absolutePath);
+    return route;
+  })
+  .forEach(route => {
+    if (route.default) {
+      route.default(app);
+    } else {
+      console.log(`Route does not have a default export: ${route}`);
+    }
+  });
 
 setupDev(app, developmentMode);
 
 // error handler
-app.use((err: HTTPError, req: express.Request, res: express.Response) => {
+app.use((err: HTTPError, req: express.Request, res: express.Response, next: express.NextFunction) => {
   console.log(err);
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.message = err?.message || 'An error occurred';
   res.locals.error = env === 'development' ? err : {};
-  res.status(err.status || 500);
+  res.status(err?.status || 500);
   res.render('error');
 });
